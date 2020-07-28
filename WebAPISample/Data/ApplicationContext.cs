8c5b9508288748875945b64ce6f220146a93646e
kadirermaya <kadir.ermaya@gmail.com>
2020-07-28 14:32:47 -0700

    Seeded data into the database.

--- WebAPISample/Data/ApplicationContext.cs
+++ WebAPISample/Data/ApplicationContext.cs
@@ -5,6 +5,8 @@ namespace WebAPISample.Data
 {
     public class ApplicationContext : DbContext
     {
+        public DbSet<Movie> Movies { get; set; }
+
         public ApplicationContext(DbContextOptions options)
             :base(options)
         {
@@ -15,9 +17,18 @@ namespace WebAPISample.Data
         {
             base.OnModelCreating(modelBuilder);
             // Seed data - needs migration
-           
+            modelBuilder.Entity<Movie>()
+               .HasData(
+                  new Movie { MovieId = 1, Title = "The Departed", Genre = "Drama", Director = "Martin Scorsese", ImagePath = "theDeparted.jpg" },
+                  new Movie { MovieId = 2, Title = "The Dark Knight", Genre = "Drama", Director = "Christopher Nolan", ImagePath = "dk.jpg" },
+                  new Movie { MovieId = 3, Title = "Inception", Genre = "Drama", Director = "Christopher Nolan", ImagePath = "inception.jpg" },
+                  new Movie { MovieId = 4, Title = "Pineapple Express", Genre = "Comedy", Director = "David Gordon Green", ImagePath = "pe.jpg" },
+                  new Movie { MovieId = 5, Title = "Die Hard", Genre = "Action", Director = "John McTiernan", ImagePath = "dieHard.jpg" }
+ );
         }
 
-        public DbSet<Movie> Movies { get; set; }
+       
     }
 }
+
+

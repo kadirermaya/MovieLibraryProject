ecd1791d17ddb2681c5cd9d69b6dca51cced5c6f
kadirermaya <kadir.ermaya@gmail.com>
2020-07-30 20:30:00 -0700

    Update details div to show card, added header and navbar, imported bootstrap theme

--- Client/app.js
+++ Client/app.js
@@ -29,7 +29,58 @@
         e.preventDefault();
     }
 
-    $('#update-form').submit(UpdateMovie);
+    $('#update-form').submit(function(e){
+        e.preventDefault();
+        UpdateMovie();
+        $('#modal-id').style.display === "none";
+    });
+        
+    
+    // $('#button').submit(function(e) {
+    //     e.preventDefault();
+    //     // Coding
+    //     $('#IDModal').modal('toggle'); //or  $('#IDModal').modal('hide');
+    //     return false;
+    // });
+
+    function SearchForm(e) {
+        let filtered = [];    
+        let searchOption = document.getElementById('select-option').value;
+        let textValue = document.getElementById('text-input').value;
+        switch (searchOption) {
+            case "title":
+                filtered = movies.filter(m => m.title.includes(textValue));
+                break;
+            case "genre":
+                filtered = movies.filter(m => m.genre.includes(textValue));
+                break;
+            case "director":
+                filtered = movies.filter(m => m.director.includes(textValue));
+                break;
+            default:
+                
+                break;
+        }
+            $('.movieData').html('');
+            $.each(filtered, function (index, value) {
+                $('.movieData').append(
+                    '<tr>' +
+                    '<td>' + value.title + '</td>' +
+                    '<td>' + value.genre + '</td>' +
+                    '<td>' + value.director + '</td>' +
+                    `<td><button type="button" value="Details" onclick="UnhideDetails(${value.movieId}, '${value.title}', '${value.genre}', '${value.director}', '${value.imagePath}')">Details</button></td>` +
+                    `<td><button type="button" value="Update" onclick="UnhideUpdate(${value.movieId}, '${value.title}', '${value.genre}', '${value.director}', '${value.imagePath}')">Update</button></td>` +
+                    '</tr>'
+                );
+
+
+            });
+            e.preventDefault();
+        };
+
+
+
+    $('#searchForm').submit(SearchForm);
 
     function processForm(e) {
         var dict = {
@@ -55,6 +106,10 @@
 
         e.preventDefault();
     }
+
+    // $('#add-movie-form').submit(function(e){
+    //     processForm();
+    // });
 
     $('#addForm').submit(processForm);
 
